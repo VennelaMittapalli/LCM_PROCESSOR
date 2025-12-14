5c87472ba25dcc2c22accd93470063da8b139fab
VennelaMittapalli <59090497+VennelaMittapalli@users.noreply.github.com>
2025-12-14 18:09:36 +0530

    Update lcm_tb.v

--- lcm_tb.v
+++ lcm_tb.v
@@ -3,7 +3,7 @@ reg clk = 1'b1;
 reg rst,go_i;
 reg [3:0]x_i;
 reg [3:0]y_i;
-wire [3:0]d_o;
+wire [7:0]d_o;
 
 always #5 clk=~clk;
 
@@ -19,3 +19,4 @@ x_i=1; y_i=2;#200;
 x_i=2;y_i=4;#200;
 #10 $finish;
 end
+endmodule
