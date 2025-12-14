7718cd4fbfa5247eeeef211d59898cb9b056e830
VennelaMittapalli <59090497+VennelaMittapalli@users.noreply.github.com>
2025-12-14 18:19:48 +0530

    Update lcm_top.v

--- lcm_top.v
+++ lcm_top.v
@@ -1,4 +1,4 @@
-module lcm_top(input rst, clk, go_i, input [3:0]x_i,y_i, output reg [7:0]d_o);
+module lcm_top(input rst, clk, go_i, input [7:0]x_i,y_i, output reg [7:0]d_o);
 wire xld,yld,xsel,ysel,enable;
 wire [1:0]comparison;
 wire [7:0]result;
